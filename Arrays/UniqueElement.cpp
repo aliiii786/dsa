5983a9b34384a3f2776e887290682b58c59381a7
Ali Ahmad Rizvi <alirizviiii2024@gmail.com>
2026-07-17 21:34:47 +0530

    Create UniqueElement.cpp

---
+++ Arrays/UniqueElement.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+int UniqueNumber(int arr[],int size){
+    int ans=0;
+    for(int i =0;i<size;i++){
+        ans=ans^arr[i];
+    }
+    return ans ;
+}
+int main() {
+    // Write C++ code here
+    int arr[]={2,2 ,3,4,4};
+    int size = sizeof(arr)/sizeof(int);
+    cout<<"Unique number in arrays is "<<UniqueNumber(arr,size);
+    
+    
+
+    return 0;
+}
